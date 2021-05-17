c8704622757b0241db9da1df10ff86f67166c139
fokamdk99 <stasskrzypek@gmail.com>
2021-05-17 15:55:48 +0200

    Add script2 and script3

--- skrypt2.sh
+++ skrypt2.sh
@@ -1,8 +1,5 @@
-
 local_directory=
 remote_directory=
-remote_user=
-remote_host=
 
 while [ "$1" != "" ]; do
     case $1 in
@@ -12,12 +9,6 @@ while [ "$1" != "" ]; do
         -rd | --remote_directory )           	shift
 				remote_directory="$1"
                                 ;;
-        -rh | --remote_host )           	shift
-                                remote_host="$1"
-                                ;;
-        -ru | --remote_user )                   shift
-                                remote_user="$1"
-				;;
 	* )			echo "provide values corresponding to the " \
 " following flags: -ld (--local_directory), -rd (--remote_directory)" \
 ", -rh (--remote_host), -ru (--remote_host)"
@@ -28,12 +19,12 @@ done
 
 echo "$local_directory"
 echo "$remote_directory"
-echo "$remote_user"
-echo "$remote_host"
-if [ -z "$local_directory" ] || [ -z "$remote_directory" ] || [ -z "$remote_host" ] || [ -z "$remote_user" ]; then
-	echo "podaj wszystkie dane"
+if [ -z "$local_directory" ] || [ -z "$remote_directory" ]; then
+	echo "provide values corresponding to the " \
+" following flags: -ld (--local_directory), -rd (--remote_directory)" \
+", -rh (--remote_host), -ru (--remote_host)"
 	exit
 fi
 
+tar cvf - "$local_directory" | (cd "$remote_directory"; tar xvf -)
 
-tar czf - "$local_directory" | ssh "$remote_user"@"$remote_host" "cd $remote_directory && tar xvzf -"
